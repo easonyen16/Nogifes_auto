a22e743f58426d87c665f99787624c7f320898f7
easonyen16 <eason990024@gmail.com>
2022-02-01 16:19:45 +0900

    Update reward_auto.bat

--- reward_auto.bat
+++ reward_auto.bat
@@ -11,10 +11,15 @@ else (aria2c "https://v2static.nogifes.jp/resource/Movie/Reward/reward_movie_0%c
 ::影片解碼
 if %cnt% LSS 1000 (crid.exe -a C5510101 -b 0013F11B -o "auto" "reward_movie_00%cnt%.usme")^
 else (crid.exe -a C5510101 -b 0013F11B -o "auto" "reward_movie_0%cnt%.usme")
+::檔案重新命名
+if %cnt% LSS 1000 (rename %~dp0\auto\*.m2v reward_movie_00%cnt%.264_med.m2v)^
+else (rename %~dp0\auto\*.m2v reward_movie_0%cnt%.264_med.m2v)
+if %cnt% LSS 1000 (rename %~dp0\auto\*.wav reward_movie_00%cnt%.avi.wav)^
+else (rename %~dp0\auto\*.wav reward_movie_0%cnt%.avi.wav)
 ::影片合併
 if %cnt% LSS 1000 (ffmpeg -i "%~dp0\auto\reward_movie_00%cnt%.264_med.m2v" -i "%~dp0\auto\reward_movie_00%cnt%.avi.wav"  -ab 320k  -y -c:v copy -c:a:0 libmp3lame "%~dp0\auto\reward_movie_00%cnt%.mp4")^
 else (ffmpeg -i "%~dp0\auto\reward_movie_0%cnt%.264_med.m2v" -i "%~dp0\auto\reward_movie_0%cnt%.avi.wav"  -ab 320k  -y -c:v copy -c:a:0 libmp3lame "%~dp0\auto\reward_movie_0%cnt%.mp4")
-::清除部必要檔案
+::清除不必要檔案
 del %~dp0\auto\*.m2v
 del %~dp0\auto\*.wav
 del %~dp0\auto\*.ini
